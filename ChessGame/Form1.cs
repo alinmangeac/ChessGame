78941a4247649953f71f7b4c5d6f5b68029d1a0b
Alin Mangeac <alin.mangeac@gmail.com>
2022-04-04 21:05:20 +0300

    Add first dynamic constructed row

--- ChessGame/Form1.cs
+++ ChessGame/Form1.cs
@@ -15,6 +15,29 @@ namespace ChessGame
         public Form1()
         {
             InitializeComponent();
+            int offset = 50;
+            int x = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                PictureBox temp = new PictureBox()
+                {
+                    Location = new Point(x, 0),
+                    Size = new Size(50, 50),
+                    BackColor = Color.Red
+                };
+
+                temp.Click += pictureBox39_Click;
+                this.Controls.Add(temp);
+                x += offset+1;
+            }
+        }
+
+
+
+        private void pictureBox39_Click(object sender, EventArgs e)
+        {
+            PictureBox temp = (PictureBox)sender;
+            MessageBox.Show(temp.Location.X.ToString());
         }
     }
 }
